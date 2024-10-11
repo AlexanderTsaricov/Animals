ff37448e400addda5ab819130096de79478dc8dd
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-10-11 14:08:51 +0500

    add create pack animal

--- AnimalsReestr/src/model/AnimalReestr.java
+++ AnimalsReestr/src/model/AnimalReestr.java
@@ -3,11 +3,13 @@ package model;
 import model.homeAnimals.Cat;
 import model.homeAnimals.Dog;
 import model.homeAnimals.Hamster;
+import model.packAnimal.Camel;
+import model.packAnimal.Donkey;
+import model.packAnimal.Horse;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class AnimalReestr {
     int animalCounter;
@@ -20,22 +22,41 @@ public class AnimalReestr {
         this.animalCounter++;
     }
     public void addHomeAnimal (
-            String name,
-            Date birthday,
-            Sex sex,
-            String command,
-            AnimalReestr reestr,
-            String voise,
-            CategoryHomeAnimal category) {
+        String name,
+        Date birthday,
+        Sex sex,
+        String command,
+        String voise,
+        CategoryHomeAnimal category) {
         switch (category){
             case Cat:
-                animalCollection.add(new Cat(name, birthday, sex,command, reestr, voise));
+                animalCollection.add(new Cat(name, birthday, sex,command, this, voise));
                 break;
             case Dog:
-                animalCollection.add(new Dog(name, birthday, sex,command, reestr, voise));
+                animalCollection.add(new Dog(name, birthday, sex,command, this, voise));
                 break;
             case Hamster:
-                animalCollection.add(new Hamster(name, birthday, sex,command, reestr, voise));
+                animalCollection.add(new Hamster(name, birthday, sex,command, this, voise));
+                break;
+        }
+    }
+    public void addPackAnimal (
+       String name,
+       Date birthday,
+       Sex sex,
+       String command,
+       String bag,
+       CategoryPackAnimal category
+    ) {
+        switch (category) {
+            case Horse:
+                animalCollection.add(new Horse(name, birthday, sex, command, this, bag));
+                break;
+            case Donkey:
+                animalCollection.add(new Donkey(name, birthday, sex, command, this, bag));
+                break;
+            case Camel:
+                animalCollection.add(new Camel(name, birthday, sex, command, this, bag));
                 break;
         }
     }
---
+++ AnimalsReestr/src/model/CategoryPackAnimal.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum CategoryPackAnimal {
+    Horse, Donkey, Camel
+}
